6aa8fac5c610e247b79dea0791d566b8d7c86f26
phartenfeller <pharti@hotmail.de>
2022-06-24 20:24:43 +0200

    shit works now

--- index.js
+++ index.js
@@ -2,41 +2,58 @@ import alfy from 'alfy';
 import { fetch, getProjectFilePath, inputMatchesData } from './lib/utils.mjs';
 
 async function main() {
-  const file = getProjectFilePath();
+  try {
+    const file = getProjectFilePath();
 
-  const projects = await fetch(file, {});
+    let projects = await fetch(file, {});
 
-  const matchedProjects = inputMatchesData(projects, alfy.input, [
-    'title',
-    'subtitle',
-  ]); // .sort((a, b) => a.title.localeCompare(b.title));
-  // .map((project) => ({
-  //   title: utils.getTitle(project),
-  //   subtitle: utils.getSubtitle(project),
-  //   icon: utils.getIcon(project),
-  //   arg: utils.getArgument(project),
-  //   valid: true,
-  //   text: {
-  //     copy: utils.getArgument(project),
-  //   },
-  // }) );
+    if (alfy.input) {
+      projects = inputMatchesData(projects, alfy.input, ['title', 'subtitle']);
+    }
+    // .sort((a, b) => a.title.localeCompare(b.title));
+    // .map((project) => ({
+    //   title: utils.getTitle(project),
+    //   subtitle: utils.getSubtitle(project),
+    //   icon: utils.getIcon(project),
+    //   arg: utils.getArgument(project),
+    //   valid: true,
+    //   text: {
+    //     copy: utils.getArgument(project),
+    //   },
+    // }) );
 
-  if (matchedProjects.length === 0) {
-    alfy.output([
-      {
-        title: 'No projects found',
-      },
-    ]);
-  } else {
-    alfy.output([
-      {
-        title: 'Hello',
-      },
-      {
-        title: 'Hello2',
-      },
-    ]);
-    /*
+    if (projects.length === 0) {
+      alfy.output([
+        {
+          title: 'No projects found',
+        },
+      ]);
+    } else {
+      // const fuck = alfy.inputMatches(projects, (item, input) =>
+      //   item.title.toLowerCase().includes(input)
+      // );
+
+      // console.log('fuck', projects);
+
+      alfy.output(
+        projects.map((p) => ({
+          title: p.title,
+          subtitle: p.subtitle,
+          // icon: p.icon,
+          arg: p.folderUri,
+          uid: p.uid,
+        }))
+      );
+
+      // alfy.output([
+      //   {
+      //     title: 'Hello',
+      //   },
+      //   {
+      //     title: 'Hello2',
+      //   },
+      // ]);
+      /*
     alfy.log('output time...');
 
     const res = matchedProjects.map((p) => ({
@@ -49,6 +66,9 @@ async function main() {
 
     alfy.output(res);
     */
+    }
+  } catch (err) {
+    console.error(`[error] ${err}`);
   }
 }
 
